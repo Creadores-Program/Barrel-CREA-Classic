047f74f807c9f70b48f0221c5593499e9db1b3fc
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2025-03-20 08:04:41 -0600

    Crear Main de Nukkit Plugin

---
+++ src/main/java/org/barrelmc/barrel/NukkitMain.java
@@ -0,0 +1,26 @@
+package org.barrelmc.barrel;
+import cn.nukkit.plugin.PluginBase;
+import org.barrelmc.barrel.network.converter.BlockConverter;
+import org.barrelmc.barrel.network.converter.ItemsConverter;
+import org.barrelmc.barrel.server.ProxyServer;
+import java.io.File;
+
+public class NukkitMain extends PluginBase{
+  private String data_pathJava;
+  private ProxyServer javaServer;
+  @Override
+  public void onEnable(){
+    this.getLogger().info("§eLoading...");
+    this.getLogger().info("Starting Barrel Proxy CREA Edition software");
+    this.getLogger().info("Barrel CREA Edition is distributed under the MIT License");
+    BlockConverter.init();
+    ItemsConverter.init();
+    getDataFolder().mkdir();
+    this.data_pathJava = getDataFolder().getAbsolutePath();
+    File fileR = new File(getDataFolder(), "config.yml");
+    if(!fileR.exist()){
+      saveResource("config.yml");
+    }
+    this.javaServer = new ProxyServer(this.data_pathJava);
+  }
+}
