7de9ace8284956eb3fc62600e505846a2c72b768
Trollhunters501PC <cpcreador@gmail.com>
2026-02-08 21:02:26 -0600

    Update and rename JavaPacketTranslator.java to ClassicPacketTranslator.java

--- src/main/java/org/barrelmc/barrel/network/translator/interfaces/JavaPacketTranslator.java
+++
@@ -1,9 +0,0 @@
-package org.barrelmc.barrel.network.translator.interfaces;
-
-import com.github.steveice10.mc.protocol.codec.MinecraftPacket;
-import org.barrelmc.barrel.player.Player;
-
-public interface JavaPacketTranslator {
-
-    void translate(MinecraftPacket pk, Player player);
-}
