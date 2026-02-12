3f712fc6fcba8aa620f843b843e0b7974beadfe8
Trollhunters501PC <cpcreador@gmail.com>
2026-02-11 20:49:33 -0600

    Update TextPacket.java

--- src/main/java/org/barrelmc/barrel/network/translator/bedrock/TextPacket.java
+++ src/main/java/org/barrelmc/barrel/network/translator/bedrock/TextPacket.java
@@ -1,7 +1,5 @@
 package org.barrelmc.barrel.network.translator.bedrock;
 
-import com.github.steveice10.mc.protocol.packet.ingame.clientbound.ClientboundSystemChatPacket;
-import net.kyori.adventure.text.Component;
 import org.barrelmc.barrel.network.translator.interfaces.BedrockPacketTranslator;
 import org.barrelmc.barrel.player.Player;
 import org.cloudburstmc.protocol.bedrock.packet.BedrockPacket;
@@ -18,14 +16,10 @@ public class TextPacket implements BedrockPacketTranslator {
                 player.sendTip(packet.getMessage());
                 break;
             }
-            case SYSTEM: {
-                player.getJavaSession().send(new ClientboundSystemChatPacket(Component.text(packet.getMessage()), false));
-                break;
-            }
             case TRANSLATION: {
                 if(player.getTraslateAd() != "true"){
                     player.setTraslateAd("true");
-                    player.sendMessage("§cChat translation not implemented! Force language on Minecraft bedrock server or use Translator for Geyser Reverse Barrel for Nukkit");
+                    player.sendMessage("&cChat translation not implemented! Force language on Minecraft bedrock server");
                 }
             }
             default: {
