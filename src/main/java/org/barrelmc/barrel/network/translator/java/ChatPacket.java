8dcaaaa8e9c2b0bd4d79b2826f526f81180feb99
Trollhunters501PC <cpcreador@gmail.com>
2026-02-13 20:31:08 -0600

    Update ChatPacket.java

--- src/main/java/org/barrelmc/barrel/network/translator/java/ChatPacket.java
+++ src/main/java/org/barrelmc/barrel/network/translator/java/ChatPacket.java
@@ -1,24 +1,44 @@
-package org.barrelmc.barrel.network.translator.java;
+package org.barrelmc.barrel.network.translator.classic;
 
-import com.github.steveice10.mc.protocol.codec.MinecraftPacket;
-import com.github.steveice10.mc.protocol.packet.ingame.serverbound.ServerboundChatPacket;
-import org.barrelmc.barrel.network.translator.interfaces.JavaPacketTranslator;
+import com.github.steveice10.packetlib.packet.Packet;
+import com.github.steveice10.mc.classic.protocol.packet.client.ClientChatPacket;
+import org.barrelmc.barrel.network.translator.interfaces.ClassicPacketTranslator;
 import org.barrelmc.barrel.player.Player;
+import org.barrelmc.barrel.server.ProxyServer;
 import org.cloudburstmc.protocol.bedrock.packet.TextPacket;
+import org.cloudburstmc.protocol.bedrock.packet.CommandRequestPacket;
+import org.cloudburstmc.protocol.bedrock.data.command.CommandOriginType;
+import org.cloudburstmc.protocol.bedrock.data.command.CommandOriginData;
+import java.util.UUID;
 
-public class ChatPacket implements JavaPacketTranslator {
+public class ChatPacket implements ClassicPacketTranslator {
 
     @Override
-    public void translate(MinecraftPacket pk, Player player) {
-        ServerboundChatPacket chatPacket = (ServerboundChatPacket) pk;
+    public void translate(Packet pk, Player player) {
+        ClientChatPacket chatPacket = (ClientChatPacket) pk;
+        player.msgPlayer += chatPacket.getMessage();
+        if(chatPacket.getUnused() == 1){
+            return;
+        }
+        if(player.msgPlayer.startsWith("/")){
+            CommandRequestPacket commandPacket = new CommandRequestPacket();
+            commandPacket.setVersion(ProxyServer.getInstance().getBedrockPacketCodec().getProtocolVersion());
+            commandPacket.setCommand("?"+player.msgPlayer.substring(1));
+            CommandOriginData Cod = new CommandOriginData(CommandOriginType.PLAYER, UUID.fromString(player.getUUID()), player.getUUID(), 0);
+            commandPacket.setCommandOriginData(Cod);
+            player.msgPlayer = "";
+            player.getBedrockClientSession().sendPacket(commandPacket);
+            return;
+        }
         TextPacket textPacket = new TextPacket();
 
         textPacket.setType(TextPacket.Type.CHAT);
         textPacket.setNeedsTranslation(false);
-        textPacket.setSourceName(chatPacket.getMessage());
-        textPacket.setMessage(chatPacket.getMessage());
+        textPacket.setSourceName(player.msgPlayer);
+        textPacket.setMessage(player.msgPlayer);
         textPacket.setXuid("");
         textPacket.setPlatformChatId("");
+        player.msgPlayer = "";
         player.getBedrockClientSession().sendPacket(textPacket);
     }
 }
