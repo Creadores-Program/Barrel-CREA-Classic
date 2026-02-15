d44412ba609a97ad17e89d7ba70084f479ec95a9
Trollhunters501PC <cpcreador@gmail.com>
2026-02-14 21:04:44 -0600

    Update PositionRotationPacket.java

--- src/main/java/org/barrelmc/barrel/network/translator/classic/PositionRotationPacket.java
+++ src/main/java/org/barrelmc/barrel/network/translator/classic/PositionRotationPacket.java
@@ -1,6 +1,7 @@
 package org.barrelmc.barrel.network.translator.classic;
 import org.barrelmc.barrel.network.translator.interfaces.ClassicPacketTranslator;
 import org.barrelmc.barrel.player.Player;
+import org.barrelmc.barrel.utils.Utils;
 import org.cloudburstmc.math.vector.Vector3f;
 import org.cloudburstmc.protocol.bedrock.data.AuthoritativeMovementMode;
 import org.cloudburstmc.protocol.bedrock.packet.MovePlayerPacket;
@@ -17,5 +18,19 @@ public class PositionRotationPacket implements ClassicPacketTranslator{
           return;
       }
       player.setOldPosition(player.getVector3f());
+      player.setPosition(Utils.mapCoords(packet.getX(), ((float) player.getMinPosClassic().getX()), ((float) player.getMaxPosClassic().getX()), ((float) player.getMinPosBedrock().getX()), ((float) player.getMaxPosBedrock().getX())), packet.getY(), Utils.mapCoords(packet.getZ(), ((float) player.getMinPosClassic().getZ()), ((float) player.getMaxPosClassic().getZ()), ((float) player.getMinPosBedrock().getZ()), ((float) player.getMaxPosBedrock().getZ())));
+      if (player.getStartGamePacketCache().getAuthoritativeMovementMode() == AuthoritativeMovementMode.CLIENT) {
+        MovePlayerPacket movePlayerPacket = new MovePlayerPacket();
+        movePlayerPacket.setRuntimeEntityId(player.getRuntimeEntityId());
+        movePlayerPacket.setPosition(player.getVector3f());
+        movePlayerPacket.setRotation(Vector3f.from(player.getPitch(), player.getYaw(), player.getYaw()));
+        movePlayerPacket.setMode(MovePlayerPacket.Mode.NORMAL);
+        movePlayerPacket.setOnGround(packet.isOnGround());
+        movePlayerPacket.setRidingRuntimeEntityId(0);
+        movePlayerPacket.setTeleportationCause(MovePlayerPacket.TeleportationCause.UNKNOWN);
+        movePlayerPacket.setEntityType(0);
+
+        player.getBedrockClientSession().sendPacket(movePlayerPacket);
+      }
     }
 }
