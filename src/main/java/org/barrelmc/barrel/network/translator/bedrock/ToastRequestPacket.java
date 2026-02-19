f139a4bb85e03b327f592271187853652d78b90c
Trollhunters501PC <cpcreador@gmail.com>
2026-02-18 20:21:55 -0600

    Update ToastRequestPacket.java

--- src/main/java/org/barrelmc/barrel/network/translator/bedrock/ToastRequestPacket.java
+++ src/main/java/org/barrelmc/barrel/network/translator/bedrock/ToastRequestPacket.java
@@ -1,11 +1,19 @@
 package org.barrelmc.barrel.network.translator.bedrock;
+import com.github.steveice10.mc.classic.protocol.data.game.PlayerIds;
 import org.barrelmc.barrel.network.translator.interfaces.BedrockPacketTranslator;
 import org.barrelmc.barrel.player.Player;
+import org.barrelmc.barrel.utils.Utils;
+import org.barrelmc.barrel.server.ProxyServer;
 import org.cloudburstmc.protocol.bedrock.packet.BedrockPacket;
 public class ToastRequestPacket implements BedrockPacketTranslator {
   @Override
   public void translate(BedrockPacket pk, Player player) {
     org.cloudburstmc.protocol.bedrock.packet.ToastRequestPacket packet = (org.cloudburstmc.protocol.bedrock.packet.ToastRequestPacket) pk;
-    player.sendTip(packet.getTitle() + "\n" + packet.getContent());
+    if(Utils.getExt(ProxyServer.getInstance().getExtDatapacks().get(8), player.getExtensionsClassic()) != null){
+      player.sendMessage(packet.getTitle(), PlayerIds.BOTTOMRIGHT2);
+      player.sendMessage(packet.getContent(), PlayerIds.BOTTOMRIGHT1);
+      return;
+    }
+    player.sendMessage(packet.getTitle() + "\n" + packet.getContent());
   }
 }
