27e1cfcc12876f5c264e30f7f5b810e2bea34020
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-02-20 13:34:33 -0600

    Implement extension checks in LevelEventPacket translation
    Added checks for player extensions before processing level events.

--- src/main/java/org/barrelmc/barrel/network/translator/bedrock/LevelEventPacket.java
+++ src/main/java/org/barrelmc/barrel/network/translator/bedrock/LevelEventPacket.java
@@ -1,9 +1,10 @@
 package org.barrelmc.barrel.network.translator.bedrock;
 
-import com.github.steveice10.mc.protocol.data.game.entity.player.BlockBreakStage;
-import com.github.steveice10.mc.protocol.packet.ingame.clientbound.level.ClientboundBlockDestructionPacket;
+import com.github.steveice10.mc.classic.protocol.packet.server.ServerEnvSetWeatherTypePacket;
 import org.barrelmc.barrel.network.translator.interfaces.BedrockPacketTranslator;
 import org.barrelmc.barrel.player.Player;
+import org.barrelmc.barrel.server.ProxyServer;
+import org.barrelmc.barrel.utils.Utils;
 import org.cloudburstmc.math.vector.Vector3f;
 import org.cloudburstmc.protocol.bedrock.data.LevelEvent;
 import org.cloudburstmc.protocol.bedrock.packet.BedrockPacket;
@@ -14,12 +15,17 @@ public class LevelEventPacket implements BedrockPacketTranslator {
     public void translate(BedrockPacket pk, Player player) {
         org.cloudburstmc.protocol.bedrock.packet.LevelEventPacket packet = (org.cloudburstmc.protocol.bedrock.packet.LevelEventPacket) pk;
 
-        if (packet.getType() == LevelEvent.BLOCK_START_BREAK) {
-            Vector3f pos = packet.getPosition();
-            player.getJavaSession().send(new ClientboundBlockDestructionPacket(0, pos.toInt(), BlockBreakStage.STAGE_1));
-        } else if (packet.getType() == LevelEvent.BLOCK_STOP_BREAK) {
-            Vector3f pos = packet.getPosition();
-            player.getJavaSession().send(new ClientboundBlockDestructionPacket(0, pos.toInt(), BlockBreakStage.RESET));
+        if(!Utils.containsExt(ProxyServer.getExtDatapacks().get(5), player.getExtensionsClassic())){
+            return;
+        }
+
+        switch(packet.getType()){
+            case LevelEvent.START_RAINING:
+            case LevelEvent.START_THUNDERSTORM:
+                break;
+            case LevelEvent.STOP_RAINING:
+            case LevelEvent.STOP_THUNDERSTORM:
+                break;
         }
     }
 }
