258ed215082af13713a9db2a5b8dc6c315433447
Trollhunters501PC <cpcreador@gmail.com>
2026-02-21 21:29:19 -0600

    Enhance SetPlayerGameTypePacket with extension check
    Added a check for extensions and send a message with the player's game mode.

--- src/main/java/org/barrelmc/barrel/network/translator/bedrock/SetPlayerGameTypePacket.java
+++ src/main/java/org/barrelmc/barrel/network/translator/bedrock/SetPlayerGameTypePacket.java
@@ -1,10 +1,9 @@
 package org.barrelmc.barrel.network.translator.bedrock;
 
-import com.github.steveice10.mc.protocol.data.game.level.notify.GameEvent;
-import com.github.steveice10.mc.protocol.packet.ingame.clientbound.level.ClientboundGameEventPacket;
-import org.barrelmc.barrel.network.translator.TranslatorUtils;
 import org.barrelmc.barrel.network.translator.interfaces.BedrockPacketTranslator;
 import org.barrelmc.barrel.player.Player;
+import org.barrelmc.barrel.server.ProxyServer;
+import org.barrelmc.barrel.utils.Utils;
 import org.cloudburstmc.protocol.bedrock.data.GameType;
 import org.cloudburstmc.protocol.bedrock.packet.BedrockPacket;
 
@@ -13,8 +12,9 @@ public class SetPlayerGameTypePacket implements BedrockPacketTranslator {
     @Override
     public void translate(BedrockPacket pk, Player player) {
         org.cloudburstmc.protocol.bedrock.packet.SetPlayerGameTypePacket packet = (org.cloudburstmc.protocol.bedrock.packet.SetPlayerGameTypePacket) pk;
-
         player.setGameMode(GameType.from(packet.getGamemode()));
-        player.getJavaSession().send(new ClientboundGameEventPacket(GameEvent.CHANGE_GAMEMODE, TranslatorUtils.translateGamemodeToJE(GameType.from(packet.getGamemode()))));
+        if(Utils.containsExt(ProxyServer.getInstance().getExtDatapacks().get(8), player.getExtensionsClassic())){
+            player.sendMessage("GameMod: " + player.getGameMode().substring(0, 1).toUpperCase() + player.getGameMode().substring(1).toLowerCase(), PlayerIds.BOTTOMRIGHT1);
+        }
     }
 }
