46bb1854cea85799173eba029cd2a8d3ad7c81e2
Trollhunters501PC <cpcreador@gmail.com>
2026-02-21 21:45:46 -0600

    Update SetBlockPacket to handle bedrock coordinates

--- src/main/java/org/barrelmc/barrel/network/translator/classic/SetBlockPacket.java
+++ src/main/java/org/barrelmc/barrel/network/translator/classic/SetBlockPacket.java
@@ -18,9 +18,13 @@ public class SetBlockPacket implements ClassicPacketTranslator{
       player.getClassicSession().send(new ServerSetBlockPacket(packet.getX(), packet.getY(), packet.getZ(), ((int) player.mapClassic[(packet.getY() * 256 + packet.getZ()) * 256 + packet.getX()])));
       return;
     }
+    player.mapClassic[(packet.getY() * 256 + packet.getZ()) * 256 + packet.getX()] = 0;
     PlayerActionPacket playerActionPacket = new PlayerActionPacket();
     playerActionPacket.setAction(PlayerActionType.DIMENSION_CHANGE_REQUEST_OR_CREATIVE_DESTROY_BLOCK);
-    Vector3i pos = new Vector3i(packet.getX(), packet.getY(), packet.getZ());
+    int bedrockX = Utils.mapCoords(packet.getX(), player.getMinPosClassic().getX(), player.getMaxPosClassic().getX(), player.getMinPosBedrock().getX(), player.getMaxPosBedrock().getX());
+    int bedrockY = packet.getY();
+    int bedrockZ = Utils.mapCoords(packet.getZ(), player.getMinPosClassic().getZ(), player.getMaxPosClassic().getZ(), player.getMinPosBedrock().getZ(), player.getMaxPosBedrock().getZ());
+    Vector3i pos = new Vector3i(bedrockX, bedrockY, bedrockZ);
     playerActionPacket.setBlockPosition(pos);
     playerActionPacket.setResultPosition(pos);
     playerActionPacket.setFace(1);
