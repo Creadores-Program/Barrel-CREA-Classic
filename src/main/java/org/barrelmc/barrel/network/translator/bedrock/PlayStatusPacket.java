15edee94b2f651e0abf1eb183a98b1d13af589af
Trollhunters501PC <cpcreador@gmail.com>
2026-02-22 21:22:39 -0600

    Implement map compression and position update logic
    Added methods for compressing map data and sending position updates.

--- src/main/java/org/barrelmc/barrel/network/translator/bedrock/PlayStatusPacket.java
+++ src/main/java/org/barrelmc/barrel/network/translator/bedrock/PlayStatusPacket.java
@@ -1,6 +1,9 @@
 package org.barrelmc.barrel.network.translator.bedrock;
 
-import com.github.steveice10.mc.protocol.packet.ingame.clientbound.entity.player.ClientboundPlayerPositionPacket;
+import com.github.steveice10.mc.classic.protocol.packet.server.ServerLevelDataPacket;
+import com.github.steveice10.mc.classic.protocol.packet.server.ServerLevelFinalizePacket;
+import com.github.steveice10.mc.classic.protocol.packet.server.ServerPositionRotationPacket;
+import com.github.steveice10.mc.classic.protocol.data.game.PlayerIds;
 import org.barrelmc.barrel.network.translator.interfaces.BedrockPacketTranslator;
 import org.barrelmc.barrel.player.Player;
 import org.cloudburstmc.math.vector.Vector2f;
@@ -9,6 +12,10 @@ import org.cloudburstmc.protocol.bedrock.data.AuthoritativeMovementMode;
 import org.cloudburstmc.protocol.bedrock.packet.BedrockPacket;
 import org.cloudburstmc.protocol.bedrock.packet.SetLocalPlayerAsInitializedPacket;
 import org.cloudburstmc.protocol.bedrock.packet.TickSyncPacket;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPOutputStream;
 
 public class PlayStatusPacket implements BedrockPacketTranslator {
 
@@ -31,13 +38,39 @@ public class PlayStatusPacket implements BedrockPacketTranslator {
                 player.startSendingPlayerInput();
             }
 
+            byte[] compressedMap = compressMap(player.mapClassic);
+            int offset = 0;
+            while(offset < compressedMap.length){
+                int length = Math.min(1024, compressedMap.length - offset);
+                byte[] chunk = new byte[length];
+                System.arraycopy(compressedMap, offset, chunk, 0, length);
+                offset += length;
+                int percent = (int) ((100L * offset) / compressedMap.length);
+                player.getClassicSession().send(new ServerLevelDataPacket(chunk, percent));
+            }
+            player.getClassicSession().send(new ServerLevelFinalizePacket(256, 256, 256));
             SetLocalPlayerAsInitializedPacket setLocalPlayerAsInitializedPacket = new SetLocalPlayerAsInitializedPacket();
             setLocalPlayerAsInitializedPacket.setRuntimeEntityId(player.getRuntimeEntityId());
             player.getBedrockClientSession().sendPacket(setLocalPlayerAsInitializedPacket);
 
             Vector3f pos = player.getLastServerPosition();
             Vector2f rotation = player.getLastServerRotation();
-            player.getJavaSession().send(new ClientboundPlayerPositionPacket(pos.getX(), pos.getY(), pos.getZ(), rotation.getY(), pos.getX(), 0));
+            float classicX = Utils.mapCoords(pos.getX(), ((float) player.getMinPosBedrock().getX()), ((float) player.getMaxPosBedrock().getX()), ((float) player.getMinPosClassic().getX()), ((float) player.getMaxPosClassic().getX()));
+            float classicY = pos.getY();
+            float classicZ = Utils.mapCoords(pos.getZ(), ((float) player.getMinPosBedrock().getZ()), ((float) player.getMaxPosBedrock().getZ()), ((float) player.getMinPosClassic().getZ()), ((float) player.getMaxPosClassic().getZ()));
+            player.getClassicSession().send(new ServerPositionRotationPacket(PlayerIds.SELF, classicX, classicY, classicZ, rotation.getX(), rotation.getY()));
         }
+    }
+    public byte[] compressMap(byte[] mapData) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        DataOutputStream dos = new DataOutputStream(baos);
+        dos.writeInt(mapData.length);
+        dos.flush();
+        ByteArrayOutputStream gzippedBase = new ByteArrayOutputStream();
+        try (GZIPOutputStream gos = new GZIPOutputStream(gzippedBase)) {
+            gos.write(mapData);
+            gos.finish();
+        }
+        return baos.toByteArray();
     }
 }
