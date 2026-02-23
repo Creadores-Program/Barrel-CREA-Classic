7ef821ed5fb423e17897c0ef13a739f40f0eff48
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-02-22 23:44:35 -0600

    Replace spawn position packet handling for classic

--- src/main/java/org/barrelmc/barrel/network/translator/bedrock/SetSpawnPositionPacket.java
+++ src/main/java/org/barrelmc/barrel/network/translator/bedrock/SetSpawnPositionPacket.java
@@ -1,6 +1,6 @@
 package org.barrelmc.barrel.network.translator.bedrock;
 
-import com.github.steveice10.mc.protocol.packet.ingame.clientbound.level.ClientboundSetDefaultSpawnPositionPacket;
+import com.github.steveice10.mc.classic.protocol.packet.server.ServerSetSpawnpointPacket;
 import org.barrelmc.barrel.network.translator.interfaces.BedrockPacketTranslator;
 import org.barrelmc.barrel.player.Player;
 import org.cloudburstmc.math.vector.Vector3i;
@@ -11,6 +11,9 @@ public class SetSpawnPositionPacket implements BedrockPacketTranslator {
     public void translate(BedrockPacket pk, Player player) {
         org.cloudburstmc.protocol.bedrock.packet.SetSpawnPositionPacket packet = (org.cloudburstmc.protocol.bedrock.packet.SetSpawnPositionPacket) pk;
         Vector3i pos = packet.getSpawnPosition();
-        player.getJavaSession().send(new ClientboundSetDefaultSpawnPositionPacket(pos, 0.0f));
+        int classicX = Utils.mapCoords(pos.getX(), player.getMinPosBedrock().getX(), player.getMaxPosBedrock().getX(), player.getMinPosClassic().getX(), player.getMaxPosClassic().getX());
+        int classicY = pos.getY();
+        int classicZ = Utils.mapCoords(pos.getZ(), player.getMinPosBedrock().getZ(), player.getMaxPosBedrock().getZ(), player.getMinPosClassic().getZ(), player.getMaxPosClassic().getZ());
+        player.getClassicSession().send(new ServerSetSpawnpointPacket(classicX, classicY, classicZ, ((int) player.yaw), ((int) player.pitch)));
     }
 }
