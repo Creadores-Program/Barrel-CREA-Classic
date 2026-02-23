5d6dbcb29edfa738a045a1feebb56ee724b0e11f
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-02-22 23:03:30 -0600

    Update max Y position in StartGamePacket

--- src/main/java/org/barrelmc/barrel/network/translator/bedrock/StartGamePacket.java
+++ src/main/java/org/barrelmc/barrel/network/translator/bedrock/StartGamePacket.java
@@ -23,7 +23,7 @@ public class StartGamePacket implements BedrockPacketTranslator {
         player.setPosition(packet.getPlayerPosition());
         player.setLastServerPosition(packet.getPlayerPosition());
         player.setLastServerRotation(packet.getRotation());
-        player.setMaxPosBedrock(new Vector3i(((int) Math.round(packet.getPlayerPosition().getX() + player.getMaxPosClassic().getX())), 249, ((int) Math.round(packet.getPlayerPosition().getZ() + player.getMaxPosBedrock().getZ()))));
+        player.setMaxPosBedrock(new Vector3i(((int) Math.round(packet.getPlayerPosition().getX() + player.getMaxPosClassic().getX())), 255, ((int) Math.round(packet.getPlayerPosition().getZ() + player.getMaxPosBedrock().getZ()))));
         player.setMinPosBedrock(new Vector3i(((int) Math.round(packet.getPlayerPosition().getX() + player.getMinPosBedrock().getX())), 0, ((int) Math.round(packet.getPlayerPosition().getZ() + player.getMinPosBedrock().getZ()))));
 
         player.setStartGamePacketCache(packet);
