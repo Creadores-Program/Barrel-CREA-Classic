47c6b26c56b22fc2583dd6753b88a05fe4bdc1ed
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-03-07 08:12:44 -0600

    Update CustomBlockSupportLevelPacket.java

--- src/main/java/org/barrelmc/barrel/network/translator/classic/CustomBlockSupportLevelPacket.java
+++ src/main/java/org/barrelmc/barrel/network/translator/classic/CustomBlockSupportLevelPacket.java
@@ -2,7 +2,6 @@ package org.barrelmc.barrel.network.translator.classic;
 import org.barrelmc.barrel.network.translator.interfaces.ClassicPacketTranslator;
 import org.barrelmc.barrel.player.Player;
 import org.barrelmc.barrel.server.ProxyServer;
-import org.barrelmc.barrel.utils.Utils;
 import com.github.steveice10.packetlib.packet.Packet;
 import com.github.steveice10.mc.classic.protocol.packet.client.ClientCustomBlockSupportLevelPacket;
 import com.github.steveice10.mc.classic.protocol.packet.server.ServerCustomBlockSupportLevelPacket;
@@ -13,9 +12,7 @@ public class CustomBlockSupportLevelPacket implements ClassicPacketTranslator{
     public void translate(Packet pk, Player player) {
         ClientCustomBlockSupportLevelPacket packet = (ClientCustomBlockSupportLevelPacket) pk;
         player.setCustomBlocksLevel(packet.getSupportLevel());
-        if(Utils.containsExt(ProxyServer.getInstance().getExtDatapacks().get(1), player.getExtensionsClassic())){
-            player.getClassicSession().send(new ServerCustomBlockSupportLevelPacket(1));
-        }
+        player.getClassicSession().send(new ServerCustomBlockSupportLevelPacket(1));
         player.getClassicSession().send(new ServerIdentificationPacket(ProxyServer.getInstance().getConfig().getMotd(), ProxyServer.getInstance().getConfig().getMotd(), UserType.NOT_OP));
         player.startSendingPing();
     }
