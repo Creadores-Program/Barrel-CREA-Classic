732d05e0a9b3273dfdb480b0a12744d1cb59da02
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-04-18 04:02:54 +0000

    Arreglar un bug de movimiento de entidad y arreglar la interaccion

--- src/main/java/org/barrelmc/barrel/network/translator/classic/PlayerClickPacket.java
+++ src/main/java/org/barrelmc/barrel/network/translator/classic/PlayerClickPacket.java
@@ -1,6 +1,8 @@
 package org.barrelmc.barrel.network.translator.classic;
+import org.cloudburstmc.protocol.bedrock.data.inventory.ItemData;
+import org.cloudburstmc.protocol.bedrock.data.inventory.transaction.InventoryTransactionType;
 import org.cloudburstmc.protocol.bedrock.packet.AnimatePacket;
-import org.cloudburstmc.protocol.bedrock.packet.InteractPacket;
+import org.cloudburstmc.protocol.bedrock.packet.InventoryTransactionPacket;
 import org.cloudburstmc.math.vector.Vector3f;
 import org.barrelmc.barrel.player.Player;
 import org.barrelmc.barrel.network.translator.interfaces.ClassicPacketTranslator;
@@ -20,17 +22,16 @@ public class PlayerClickPacket implements ClassicPacketTranslator{
         if(packet.getTargetEntityID() < 0){
             return;
         }
-        InteractPacket bedrockpacket = new InteractPacket();
-        bedrockpacket.setRuntimeEntityId((long) packet.getTargetEntityID());
-        switch(packet.getButton()){
-            case 0:
-                bedrockpacket.setAction(InteractPacket.Action.DAMAGE);
-                break;
-            case 1:
-                bedrockpacket.setAction(InteractPacket.Action.INTERACT);
-                break;
-        }
-        bedrockpacket.setMousePosition(Vector3f.ZERO);
-        player.getBedrockClientSession().sendPacket(bedrockpacket);
+        InventoryTransactionPacket bepk = new InventoryTransactionPacket();
+        bepk.setTransactionType(InventoryTransactionType.ITEM_USE_ON_ENTITY);
+        bepk.setRuntimeEntityId((long) packet.getTargetEntityID());
+        bepk.setActionType(packet.getButton() == 0 ? 1 : 0);
+        Vector3f pos = player.getVector3f();
+        bepk.setPlayerPosition(pos);
+        bepk.setHotbarSlot(player.getHotbarSlot());
+        bepk.setItemInHand(ItemData.AIR);
+        bepk.setHeadPosition(pos);
+        bepk.setClickPosition(Vector3f.ZERO);
+        player.getBedrockClientSession().sendPacket(bepk);
     }
 }
